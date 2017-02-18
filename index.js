44ab72c342b44e4d1e1c91d6cd74cab032322ca5
Rodrigo Quelca <rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-02-18 02:21:39 -0400

    test template

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const wit = bot.getWit();
 // Webserver parameter
 const PORT = process.env.PORT || 8445;
 
+
+// Get content from file
+var contents = fs.readFileSync("./config.json");
+// Define to JSON type
+var config = JSON.parse(contents);
+
 // Wit.ai bot specific code
 
 // This will contain all user sessions.
@@ -104,34 +110,77 @@ app.post('/webhook', (req, res) => {
         'Sorry I can only process text messages for now.'
       );
     } else if (msg) {
+
       // We received a text message
 
-      // Let's forward the message to the Wit.ai Bot Engine
-      // This will run all actions until our bot has nothing left to do
-      wit.runActions(
-        sessionId, // the user's current session
-        msg, // the user's message 
-        Config.sessions[sessionId].context, // the user's current session state
-        (error, context) => {
-          if (error) {
-            console.log('Oops! Got an error from Wit:', error);
-          } else {
-            // Our bot did everything it has to do.
-            // Now it's waiting for further messages to proceed.
-            console.log('Waiting for futher messages.');
+      let tplObj = _.find(config, function (o) { return o.message === text});
 
-            // Based on the session state, you might want to reset the session.
-            // This depends heavily on the business logic of your bot.
-            // Example:
-            // if (context['done']) {
-            //   delete sessions[sessionId];
-            // }
-
-            // Updating the user's current session state
-            sessions[sessionId].context = context;
+      if (msg === 'plantilla') {
+          // const recipientId = sessions[sessionId].fbid;
+          
+          let tpl = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "airline_update",
+        "intro_message": "Your flight is delayed",
+        "update_type": "delay",
+        "locale": "en_US",
+        "pnr_number": "CF23G2",
+        "update_flight_info": {
+          "flight_number": "KL123",
+          "departure_airport": {
+            "airport_code": "SFO",
+            "city": "San Francisco",
+            "terminal": "T4",
+            "gate": "G8"
+          },
+          "arrival_airport": {
+            "airport_code": "AMS",
+            "city": "Amsterdam",
+            "terminal": "T4",
+            "gate": "G8"
+          },
+          "flight_schedule": {
+            "boarding_time": "2015-12-26T10:30",
+            "departure_time": "2015-12-26T11:30",
+            "arrival_time": "2015-12-27T07:30"
           }
         }
-      );
+      }
+    }
+  };
+           FB.fbMessage2(recipientId, tpl);
+      }  else {
+          // Let's forward the message to the Wit.ai Bot Engine
+          // This will run all actions until our bot has nothing left to do
+          wit.runActions(
+            sessionId, // the user's current session
+            msg, // the user's message 
+            Config.sessions[sessionId].context, // the user's current session state
+            (error, context) => {
+              if (error) {
+                console.log('Oops! Got an error from Wit:', error);
+              } else {
+                // Our bot did everything it has to do.
+                // Now it's waiting for further messages to proceed.
+                console.log('Waiting for futher messages.');
+
+                // Based on the session state, you might want to reset the session.
+                // This depends heavily on the business logic of your bot.
+                // Example:
+                // if (context['done']) {
+                //   delete sessions[sessionId];
+                // }
+
+                // Updating the user's current session state
+                sessions[sessionId].context = context;
+              }
+            }
+          );
+      }
+
+     
     }
   }
   res.sendStatus(200);
