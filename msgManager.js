223807792f72310bb34540535485568ba5468100
Rodrigo <rodrigoquelca@gmail.com>
2017-03-17 21:26:38 -0400

    remove forced template

--- msgManager.js
+++ msgManager.js
@@ -8,10 +8,6 @@ const first = require('./tpls/first.js');
 // Setting up our bot
 const wit = bot.getWit();
 
-// Webserver parameter
-const PORT = process.env.PORT || 8445;
-
-
 // Wit.ai bot specific code
 
 // This will contain all user sessions.
@@ -20,97 +16,86 @@ const PORT = process.env.PORT || 8445;
 
 
 const findOrCreateSession = (fbid) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(Config.sessions).forEach(k => {
-    if (Config.sessions[k].fbid === fbid) {
-      // Yep, got it!
-      sessionId = k;
+    let sessionId;
+    // Let's see if we already have a session for the user fbid
+    Object.keys(Config.sessions).forEach(k => {
+        if (Config.sessions[k].fbid === fbid) {
+            // Yep, got it!
+            sessionId = k;
+        }
+    });
+    if (!sessionId) {
+        // No session found for user fbid, let's create a new one
+        sessionId = new Date().toISOString();
+        Config.sessions[sessionId] = {
+            fbid: fbid,
+            context: {
+                _fbid_: fbid
+            }
+        }; // set context, _fid_
     }
-  });
-  if (!sessionId) {
-    // No session found for user fbid, let's create a new one
-    sessionId = new Date().toISOString();
-    Config.sessions[sessionId] = {
-      fbid: fbid,
-      context: {
-        _fbid_: fbid
-      }
-    }; // set context, _fid_
-  }
-  return sessionId;
+    return sessionId;
 };
 
 
 
 const msgProcess = (req, res) => {
-  // Parsing the Messenger API response
-  const messaging = FB.getFirstMessagingEntry(req.body);
+    // Parsing the Messenger API response
+    const messaging = FB.getFirstMessagingEntry(req.body);
 
-  if (messaging && messaging.message) {
-   console.log ('messaging.message' + messaging.message)
-    // Yay! We got a new message!
+    if (messaging && messaging.message) {
+        // Yay! We got a new message!
 
-    // We retrieve the Facebook user ID of the sender
-    const sender = messaging.sender.id;
+        // We retrieve the Facebook user ID of the sender
+        const sender = messaging.sender.id;
 
-    // We retrieve the user's current session, or create one if it doesn't exist
-    // This is needed for our bot to figure out the conversation history
-    const sessionId = findOrCreateSession(sender);
+        // We retrieve the user's current session, or create one if it doesn't exist
+        // This is needed for our bot to figure out the conversation history
+        const sessionId = findOrCreateSession(sender);
 
-    // We retrieve the message content
-    const msg = messaging.message.text;
-    const atts = messaging.message.attachments;
+        // We retrieve the message content
+        const msg = messaging.message.text;
+        const atts = messaging.message.attachments;
 
-    if (atts) {
-      // We received an attachment
+        if (atts) {
+            // We received an attachment
 
-      // Let's reply with an automatic message
-      FB.fbMessage(
-        sender,
-        'Sorry I can only process text messages for now.'
-      );
-    } else if (msg) {
+            // Let's reply with an automatic message
+            FB.fbMessage(
+                sender,
+                'Sorry I can only process text messages for now.'
+            );
+        } else if (msg) {
+            // Let's forward the message to the Wit.ai Bot Engine
+            // This will run all actions until our bot has nothing left to do
+            wit.runActions(
+                sessionId, // the user's current session
+                msg, // the user's message 
+                Config.sessions[sessionId].context, // the user's current session state
+                (error, context) => {
+                    if (error) {
+                        console.log('Oops! Got an error from Wit:', error);
+                    } else {
+                        // Our bot did everything it has to do.
+                        // Now it's waiting for further messages to proceed.
+                        console.log('Waiting for futher messages.');
 
-      // We received a text message
-      if (msg === 'template') {
-         FB.fbMessage(
-          sender,
-          'aqui se procesa el template'
-        );
-      } else {
-          // Let's forward the message to the Wit.ai Bot Engine
-          // This will run all actions until our bot has nothing left to do
-          wit.runActions(
-            sessionId, // the user's current session
-            msg, // the user's message 
-            Config.sessions[sessionId].context, // the user's current session state
-            (error, context) => {
-              if (error) {
-                console.log('Oops! Got an error from Wit:', error);
-              } else {
-                // Our bot did everything it has to do.
-                // Now it's waiting for further messages to proceed.
-                console.log('Waiting for futher messages.');
+                        // Based on the session state, you might want to reset the session.
+                        // This depends heavily on the business logic of your bot.
+                        // Example:
+                        // if (context['done']) {
+                        //   delete sessions[sessionId];
+                        // }
 
-                // Based on the session state, you might want to reset the session.
-                // This depends heavily on the business logic of your bot.
-                // Example:
-                // if (context['done']) {
-                //   delete sessions[sessionId];
-                // }
-
-                // Updating the user's current session state
-                sessions[sessionId].context = context;
-              }
-            }
-          );
-      }
-
+                        // Updating the user's current session state
+                        sessions[sessionId].context = context;
+                    }
+                }
+            );
+        }
     }
-  }
 }
-// public sources
+
 module.exports = {
-  msgProcess: msgProcess
+    msgProcess: msgProcess
 };
