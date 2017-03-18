312eef1425574568b3143fd7d4a8faed66850035
Rodrigo <rodrigoquelca@gmail.com>
2017-03-18 16:28:44 -0400

    fix context

--- bot.js
+++ bot.js
@@ -86,7 +86,8 @@ const actions = {
 
       // context.template = JSON.stringify(tpl); // we should call a weather API here
       console.log('init the process');
-      var result = JSON.stringify(factory.getWeather);
+      cosole.log(actory.getWeather):
+      context.template = JSON.stringify(factory.getWeather);
     }
     console.log('---all has been finixed---');
     console.log(context.template);
