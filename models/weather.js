68ce306e2bb69bc740de66542d3362618c3a48f6
Rodrigo <rodrigoquelca@gmail.com>
2017-03-19 10:16:50 -0400

    weather 29

--- models/weather.js
+++ models/weather.js
@@ -18,12 +18,47 @@ const wheather = (config, id, cb) => {
             res = JSON.parse(res);
             //     tpl = res;
             // console.log(res[1].current.temperature);
-            // tpl = processTpl(res);
-            cb(res[0].current.temperature + '  grados');
+            tpl = processTpl(res);
+            cb(tpl);
         });
     });
 };
-
+const processTpl = (res) => {
+    return tpl = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "airline_checkin",
+        "intro_message": "Check-in is available now.",
+        "locale": "en_US",
+        "pnr_number": "ABCDEF",
+        "flight_info": [
+          {
+            "flight_number": "f001",
+            "departure_airport": {
+              "airport_code": "SFO",
+              "city": "San Francisco",
+              "terminal": "T4",
+              "gate": "G8"
+            },
+            "arrival_airport": {
+              "airport_code": "SEA",
+              "city": "Seattle",
+              "terminal": "T4",
+              "gate": "G8"
+            },
+            "flight_schedule": {
+              "boarding_time": "2016-01-05T15:05",
+              "departure_time": "2016-01-05T15:45",
+              "arrival_time": "2016-01-05T17:30"
+            }
+          }
+        ],
+        "checkin_url": "https:\/\/www.airline.com\/check-in"
+      }
+    }
+  }
+};
 
 module.exports = {
     wheather: wheather
