bf63b6a33badb3057ea071fc48e77bf09314c2cf
Rodrigo <rodrigoquelca@gmail.com>
2017-03-19 00:04:32 -0400

    init process 2

--- msgManager.js
+++ msgManager.js
@@ -96,6 +96,7 @@ const msgProcess = (req, res) => {
     } else if (messaging && messaging.postback.payload) {
         console.log('start process procesdure');
         var res = str.split(",");
+        console.log(res);
         switch (res[0]) {
             case 'startProcess':
                 FB.fbMessage(
