02614d5054966e6fb9627a9aed0b261f76c604b7
Rodrigo <rodrigoquelca@gmail.com>
2017-03-19 00:48:00 -0400

    init process 13

--- facebook.js
+++ facebook.js
@@ -44,6 +44,27 @@ const fbMessage = (id, text, hasTpl) => {
   });
 };
 
+const fbMessageReply = (id, text, payload, hasTpl) => {
+  console.log('el id es el siguiente: ' +id)
+  const body = JSON.stringify({
+    recipient: { id },
+    message: hasTpl ? text: { text },
+    quick_reply: {payload: payload}
+  }); 
+  const qs = 'access_token=' + encodeURIComponent(Config.FB_PAGE_TOKEN);
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
+    }
+    return json;
+  });
+};
 // See the Webhook reference
 // https://developers.facebook.com/docs/messenger-platform/webhook-reference
 const getFirstMessagingEntry = (body) => {
